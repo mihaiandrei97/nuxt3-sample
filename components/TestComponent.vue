b86a95e2df18775ce36f6d26e1e740abbcaa6be8
Mihai Andrei <mihai.andrei@modex.tech>
2021-12-13 18:20:30 +0200

    added append to index

---
+++ components/TestComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Hello from TestComponent</div>
+</template>
--- generate/component/index.js
+++ generate/component/index.js
@@ -14,6 +14,18 @@ module.exports = {
         path: "components/{{pascalCase name}}.vue",
         templateFile: "./generate/component/templates/component.hbs",
       },
+       {
+                path: 'components/index.js',
+                pattern: /(\/\/ COMPONENT IMPORTS)/g,
+                template: 'import {{pascalCase name}} from \'./components/{{pascalCase name}}\';\n$1',
+                type: 'modify',
+            },
+            {
+                path: 'components/index.js',
+                pattern: /(\/\/ COMPONENT EXPORTS)/g,
+                template: '\t{{pascalCase name}},\n$1',
+                type: 'modify',
+            },
     ];
   },
 };
